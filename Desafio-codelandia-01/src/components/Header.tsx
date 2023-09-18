3b1ce210882c0c2ca52cd65bb094fe025084858f
Samuel Souza <smksouza.dev@gmail.com>
2023-09-18 13:55:21 -0300

    initial commit

---
+++ Desafio-codelandia-01/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="header-content">
+        <div className="logo">
+          <h2>Codelândia</h2>
+          <h2>blog</h2>
+        </div>
+      </div>
+    </header>
+  );
+};
